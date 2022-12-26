b21605d25d1a3e680e20d27d3c03b0c72c39d67a
yyamada12 <12yacropolisy@gmail.com>
2022-12-26 15:44:11 +0900

    chore: add jest and react-testing-library

--- next.config.js
+++ next.config.js
@@ -1,9 +1,20 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  compiler: {
-    styledComponents: true,
-  },
+  compiler: (() => {
+    let compilerConfig = {
+      styledComponents: true,
+    }
+    if (process.env.NODE_ENV === 'production') {
+      compilerConfig = {
+        ...compilerConfig,
+        // 本番環境ではReact Testing Libraryで使用するdata-testid属性を削除
+        reactRemoveProperties: { properties: ['^data-testid$'] },
+      }
+    }
+
+    return compilerConfig
+  })(),
 }
 
 module.exports = nextConfig
