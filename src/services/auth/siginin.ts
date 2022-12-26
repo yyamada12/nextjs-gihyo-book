36c14c913639c8a5cee61080ad4d55f4f5154592
yyamada12 <12yacropolisy@gmail.com>
2022-12-26 18:38:35 +0900

    feat: add services for api call

---
+++ src/services/auth/siginin.ts
@@ -0,0 +1,41 @@
+// typesは後ほど定義
+import { ApiContext, User } from 'types'
+import { fetcher } from 'utils'
+
+export type SigninParams = {
+  /**
+   * ユーザー名
+   * サンプルユーザーのユーザー名は "user"
+   */
+  username: string
+  /**
+   * パスワード
+   * サンプルユーザーのパスワードは "password"
+   */
+  password: string
+}
+
+/**
+ * 認証API（サインイン）
+ * @param context APIコンテキスト
+ * @param params パラメータ
+ * @returns ログインユーザー
+ */
+const signin = async (
+  context: ApiContext,
+  params: SigninParams,
+): Promise<User> => {
+  return await fetcher(
+    `${context.apiRootUrl.replace(/\/$/g, '')}/auth/signin`,
+    {
+      method: 'POST',
+      headers: {
+        Accept: 'application/json',
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(params),
+    },
+  )
+}
+
+export default signin
